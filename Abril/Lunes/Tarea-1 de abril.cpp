c01ec2a48a091ae77a2c405ced613a2f6a90d416
EduardoAntillon <eduardorafaelantillonjerez@gmail.com>
2024-04-01 15:50:27 -0600

    Cambios

---
+++ Abril/Lunes/Tarea-1 de abril.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i, lim, x = 0, y = 1, z = 1;
+	
+	cout<<"Escribir un numero como numero de terminos de la serie de Fibonacci: ";
+	cin>>lim;
+	cout<<"La sucesion es: ";
+	cout<<"0, 1, ";
+	
+	for(i=1;i<=lim-2;i++){
+		z = x + y;
+		
+		x = y;
+		y = z;
+		
+		if(i==lim-2){
+			cout<<z;
+		}
+ 		else{
+			cout<<z<<", ";
+		}
+	}
+	
+	return 0;
+}
+
